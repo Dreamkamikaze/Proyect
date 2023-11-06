7c3930e03f39cb68d7e36472b8f85ab93c8a60ea
Dream Kamikaze <141891655+Dreamkamikaze@users.noreply.github.com>
2023-11-06 16:14:28 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config();
+
+(async() => {
+  try {
+    await mongoose.connect(process.env.MONGO_DEV_URI);
+    console.log('Conecto a MongoDB');
+  } catch (error) {
+    console.log(error);
+    console.log('no conecto a mongodb');
+  }
+})();
+
+//Rutas Frontend
+app.use('/styles', express.static(path.resolve(__dirname, 'views', 'styles')));
+app.use('/', express.static(path.resolve(__dirname, 'views', 'home')));
+app.use('/signup', express.static(path.resolve(__dirname, 'views', 'signup')));
+
+//Rutas Backend
+
+module.exports = app;
